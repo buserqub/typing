713d5a4343ec462d201cda48ea05d464753b0a8b
buserqub <73015330+buserqub@users.noreply.github.com>
2023-06-14 13:46:58 +0500

    Добавлена подсветка нажатых клавиш

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function buildRusKeyboard () {
+	let keyBoard = document.createElement('div');
+}
+
+function clear() {
+	let key = document.getElementsByClassName("pressed_key");
+	if (key[0] != null) {
+		let elem = document.createElement('div');
+		elem.className = "key";
+		elem.id = key[0].id;
+		elem.innerHTML = key[0].innerHTML.toUpperCase();
+		key[0].replaceWith(elem);
+	}
+}
+
+
+document.onkeypress = function (e) {
+    clear();
+    let elem = document.createElement('div');
+    elem.className = "pressed_key";
+    elem.id = String(e.key.toUpperCase());
+    elem.innerHTML = e.key.toUpperCase();
+    let key = document.getElementById(String(e.key.toUpperCase()));
+    key.replaceWith(elem);
+};
