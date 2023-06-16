27fbb0769fd9f8791f9a220a11fdb809a4f18ce5
buserqub <sashok-59@bk.ru>
2023-06-16 11:39:33 +0500

    Добавил счетчик скорости и ошибок

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,9 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function buildRusKeyboard () {
-	let keyBoard = document.createElement('div');
+
+startdate = new Date();
+	clockStart = startdate.getTime();
+function refreshStopwatch() {
+	startdate = new Date();
+	clockStart = startdate.getTime();
 }
+function initStopwatch() { 
+	var thisTime = new Date();
+	console.log((thisTime.getTime() - clockStart)/1000);
+	return (thisTime.getTime() - clockStart)/1000;
+}
+function getSecs() { 
+	var tSecs = (initStopwatch()); 
+	var iSecs = tSecs % 60;
+	var iMins = ((tSecs-30)/60); 
+	var sSecs ="" + ((iSecs > 9) ? iSecs : "0" + iSecs);
+	var sMins ="" + ((iMins > 9) ? iMins : "0" + iMins);
+	document.getElementById("timer-counter").innerHTML = sMins+":"+sSecs;
+	setTimeout('getSecs()', 1000); 
+}
+
 
 function clear() {
 	let key = document.getElementsByClassName("pressed_key");
@@ -24,6 +43,7 @@ function moveCursor (e) {
 		cursor = document.getElementById("typing_wrong");
 
 	if (e.key == cursor.innerHTML) {
+		typedSymbols++;
 		let typed = document.getElementById("typed");
 		let text = document.getElementById("text");
 
@@ -35,6 +55,7 @@ function moveCursor (e) {
 		if (cursor.innerHTML == "")
 			refreshText();
 	} else {
+		errors++;
 		cursor.id = "typing_wrong";
 	}
 }
@@ -47,10 +68,21 @@ function refreshText () {
 	text.innerHTML = typed.innerHTML.slice(1);
 	typed.innerHTML = "";
 }
-
+typedSymbols = 0;
+errors = 0;
 document.onkeypress = function (e) {
+	let typed = document.getElementById("typed");
+	if (typed.innerHTML == "") {
+		refreshStopwatch();
+		typedSymbols = 0;
+		errors = 0;
+	}
 	console.log(e.key);
 	moveCursor(e);
+	if (typedSymbols != 1)
+		document.getElementById("speed").innerHTML = String(Math.round(typedSymbols / initStopwatch() * 60));
+	document.getElementById("accuracy").innerHTML = String(Math.round((typedSymbols - errors) / typedSymbols * 100));
+	console.log(typedSymbols, errors);
 	clear();
 	let elem = document.createElement('div');
 	elem.className = "pressed_key";
