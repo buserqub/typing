93866d75798067464e751630e46f4ebf8eebc5cd
buserqub <sashok-59@bk.ru>
2023-06-29 10:29:05 +0500

    Добавил страницу для английской раскладки

--- js/index2.js
+++ js/index2.js
@@ -1,10 +1,10 @@
 ReactDOM.render(
     <div>
-        <h1 class = "main_text"><span id = "typed"></span><span id = "typing">Н</span><span id = "text">ачните печать! Не останавливайтесь! Продолжайте! Уже почти! Вы близки к цели! Победа!</span></h1>
+        <h1 class = "main_text"><span id = "typed"></span><span id = "typing">S</span><span id = "text">tart typing! Dont stop! Continue! Almost there! You are close to the point! Victory!</span></h1>
 	    <br></br>
 	    <div class = "keyboard" id ="keyboard">
             <div class = "keyboard_row" id ="row_1">
-                <div class = "key" id = "Ё">Ё</div>
+                <div class = "key" id = "~">~</div>
                 <div class = "key" id = "1">1</div>
                 <div class = "key" id = "2">2</div>
                 <div class = "key" id = "3">3</div>
@@ -21,49 +21,49 @@ ReactDOM.render(
             </div>
             <div class = "keyboard_row"  id ="row_2">
                 <div class = "flex_key" id = "TAB">Tab</div>
-                <div class = "key" id = "Й">Й</div>
-                <div class = "key" id = "Ц">Ц</div>
-                <div class = "key" id = "У">У</div>
-                <div class = "key" id = "К">К</div>
-                <div class = "key" id = "Е">Е</div>
-                <div class = "key" id = "Н">Н</div>
-                <div class = "key" id = "Г">Г</div>
-                <div class = "key" id = "Ш">Ш</div>
-                <div class = "key" id = "Щ">Щ</div>
-                <div class = "key" id = "З">З</div>
-                <div class = "key" id = "Х">Х</div>
-                <div class = "key" id = "Ъ">Ъ</div>
-                <div class = "flex_key" id = "/">/</div>
+                <div class = "key" id = "Q">Q</div>
+                <div class = "key" id = "W">W</div>
+                <div class = "key" id = "E">E</div>
+                <div class = "key" id = "R">R</div>
+                <div class = "key" id = "T">T</div>
+                <div class = "key" id = "Y">Y</div>
+                <div class = "key" id = "U">U</div>
+                <div class = "key" id = "I">I</div>
+                <div class = "key" id = "O">O</div>
+                <div class = "key" id = "P">P</div>
+                <div class = "key" id = "{">{"{"}</div>
+                <div class = "key" id = "}">{"}"}</div>
+                <div class = "flex_key" id = "|">|</div>
             </div>
             <div class = "keyboard_row" id ="row_3">
                 <div class = "flex_key" id = "CL">CapsLock</div>
-                <div class = "key" id = "Ф">Ф</div>
-                <div class = "key" id = "Ы">Ы</div>
-                <div class = "key" id = "В">В</div>
-                <div class = "key" id = "А">А</div>
-                <div class = "key" id = "П">П</div>
-                <div class = "key" id = "Р">Р</div>
-                <div class = "key" id = "О">О</div>
-                <div class = "key" id = "Л">Л</div>
-                <div class = "key" id = "Д">Д</div>
-                <div class = "key" id = "Ж">Ж</div>
-                <div class = "key" id = "Э">Э</div>
+                <div class = "key" id = "A">A</div>
+                <div class = "key" id = "S">S</div>
+                <div class = "key" id = "D">D</div>
+                <div class = "key" id = "F">F</div>
+                <div class = "key" id = "G">G</div>
+                <div class = "key" id = "H">H</div>
+                <div class = "key" id = "J">J</div>
+                <div class = "key" id = "K">K</div>
+                <div class = "key" id = "L">L</div>
+                <div class = "key" id = ":">:</div>
+                <div class = "key" id = "&quot;">&quot;</div>
                 <div class = "flex_key" id = "ENTER">Enter</div>
-                </div>
+            </div>
             <div class = "keyboard_row"  id ="row_4">
                 <div class = "flex_key" id = "LSHIFT">Shift</div>
-                <div class = "key" id = "Я">Я</div>
-                <div class = "key" id = "Ч">Ч</div>
-                <div class = "key" id = "С">С</div>
-                <div class = "key" id = "М">М</div>
-                <div class = "key" id = "И">И</div>
-                <div class = "key" id = "Т">Т</div>
-                <div class = "key" id = "Ь">Ь</div>
-                <div class = "key" id = "Б">Б</div>
-                <div class = "key" id = "Ю">Ю</div>
-                <div class = "key" id = ",">,</div>
+                <div class = "key" id = "Z">Z</div>
+                <div class = "key" id = "X">X</div>
+                <div class = "key" id = "C">C</div>
+                <div class = "key" id = "V">V</div>
+                <div class = "key" id = "B">B</div>
+                <div class = "key" id = "N">N</div>
+                <div class = "key" id = "M">M</div>
+                <div class = "key" id = "<">{"<"}</div>
+                <div class = "key" id = ">">{">"}</div>
+                <div class = "key" id = "?">?</div>
                 <div class = "flex_key" id = "RSHIFT">Shift</div>
-                </div>
+            </div>
             <div class = "keyboard_row" id ="row_5">
                 <div class = "flex_key" id = "LCTRL">Ctrl</div>
                 <div class = "flex_key" id = "WIN">Win</div>
@@ -72,14 +72,14 @@ ReactDOM.render(
                 <div class = "flex_key" id = "RALT">Alt</div>
                 <div class = "flex_key" id = "FN">Fn</div>
                 <div class = "flex_key" id = "RCTRL">Ctrl</div>
-                </div>
+            </div>
         </div>
         <div class = "stats">
             <div class = "speed">
-                <h2><span>Скорость: </span><span id = "speed">-</span><span> СPM </span><img  title = "CPM (Clicks Per Minute) - стандартная единица измерения скорости, характеризующаяся частотой нажатий клавиш в минуту" class = "sign" src = "question.jpg"></img></h2>
+                <h2><span>Speed: </span><span id = "speed">-</span><span> СPM </span><img  title = "CPM (Clicks Per Minute) - standart unit of measurement, characterized by frequency of clicks per minute" class = "sign" src = "question.jpg"></img></h2>
             </div>
             <div class = "speed">
-                <h2><span>Точность: </span><span id = "accuracy">-</span><span> % </span><img  title = "Процент правильно введенных символов от общего количества нажатий клавиш" class = "sign" src = "question.jpg"></img></h2>
+                <h2><span>Accuracy: </span><span id = "accuracy">-</span><span> % </span><img  title = "Percent right typed symbols from the all count of clicks" class = "sign" src = "question.jpg"></img></h2>
             </div>
         </div>
         <br></br>
@@ -126,24 +126,64 @@ function clear() {
 }
 function toUpper () {
 	let keys = document.getElementsByClassName("key");
+    console.log(keys.length);
 	for (let i = 0; i < keys.length; i++) {
 		keys[i].innerHTML = keys[i].innerHTML.toUpperCase();
 		keys[i].id = keys[i].innerHTML;
+        console.log(keys[i].id);
 	}
-	let string = "Ё!\"№;%:?*()_+";
+	let string = "~!\"№;%:?*()_+";
 	for (let i = 0; i < string.length; i++) {
 		keys[i].innerHTML = string[i];
 		keys[i].id = keys[i].innerHTML;
 	}
-	keys = document.getElementById(",");
+    keys = document.getElementById("{");
 	if (keys == null)
-		keys = document.getElementById(".");
-	keys.innerHTML = ",";
+		keys = document.getElementById("[");
+	keys.innerHTML = "{";
 	keys.id = keys.innerHTML;
 
-	keys = document.getElementsByClassName("flex_key");
-	keys[2].innerHTML = "/";
-	keys[2].id = keys.innerHTML;
+    keys = document.getElementById("}");
+	if (keys == null)
+		keys = document.getElementById("]");
+	keys.innerHTML = "}";
+	keys.id = keys.innerHTML;
+
+    keys = document.getElementById(":");
+	if (keys == null)
+		keys = document.getElementById(";");
+	keys.innerHTML = ":";
+	keys.id = keys.innerHTML;
+
+    keys = document.getElementById("\"");
+	if (keys == null)
+		keys = document.getElementById("\'");
+	keys.innerHTML = "\"";
+	keys.id = keys.innerHTML;
+
+	keys = document.getElementById("&lt;");
+	if (keys == null)
+		keys = document.getElementById(",");
+	keys.innerHTML = "<";
+	keys.id = keys.innerHTML;
+
+    keys = document.getElementById("&gt;");
+	if (keys == null)
+		keys = document.getElementById(".");
+	keys.innerHTML = ">";
+	keys.id = keys.innerHTML;
+
+    keys = document.getElementById("?");
+	if (keys == null)
+		keys = document.getElementById("/");
+	keys.innerHTML = "?";
+	keys.id = keys.innerHTML;
+
+    keys = document.getElementById("|");
+	if (keys == null)
+		keys = document.getElementById("\\");
+	keys.innerHTML = "|";
+	keys.id = keys.innerHTML;
 
 	keys = document.getElementById("LSHIFT");
 	highlightSpecial(keys.innerHTML, keys.className, keys.id);
@@ -159,15 +199,55 @@ function toLower () {
 		keys[i].innerHTML = string[i];
 		keys[i].id = keys[i].innerHTML;
 	}
-	keys = document.getElementById(".");
+	
+    keys = document.getElementById("[");
 	if (keys == null)
-		keys = document.getElementById(",");
+        keys = document.getElementById("{");
+	keys.innerHTML = "[";
+	keys.id = keys.innerHTML;
+
+    
+    keys = document.getElementById("]");
+	if (keys == null)
+        keys = document.getElementById("}");
+	keys.innerHTML = "]";
+	keys.id = keys.innerHTML;
+
+    keys = document.getElementById(";");
+	if (keys == null)
+        keys = document.getElementById(":");
+	keys.innerHTML = ";";
+	keys.id = keys.innerHTML;
+
+    keys = document.getElementById("\'");
+	if (keys == null)
+        keys = document.getElementById("\"");
+	keys.innerHTML = "\'";
+	keys.id = keys.innerHTML;
+
+    keys = document.getElementById(",");
+	if (keys == null)
+        keys = document.getElementById("&lt;");
+	keys.innerHTML = ",";
+	keys.id = keys.innerHTML;
+
+    keys = document.getElementById(".");
+	if (keys == null)
+        keys = document.getElementById("&gt;");
 	keys.innerHTML = ".";
 	keys.id = keys.innerHTML;
 
-	keys = document.getElementsByClassName("flex_key");
-	keys[2].innerHTML = "\\";
-	keys[2].id = keys.innerHTML;
+    keys = document.getElementById("/");
+	if (keys == null)
+        keys = document.getElementById("?");
+	keys.innerHTML = "/";
+	keys.id = keys.innerHTML;
+
+    keys = document.getElementById("\\");
+	if (keys == null)
+    keys = document.getElementById("|");
+	keys.innerHTML = "\\";
+	keys.id = keys.innerHTML;
 	
 }
 function moveCursor (e) {
