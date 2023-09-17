b6a6f183db3dba89349e8e0e8e19f2572c8ba35f
buserqub <sashok-59@bk.ru>
2023-09-17 23:36:09 +0500

    Правки под gh pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Routes, Route, HashRouter, BrowserRouter} from 'react-router-dom';
 import Home from './Home';
 import RusTrainer from './RusTrainer';
 import EngTrainer from './EngTrainer';
 import NavBar from './NavBar';
-currentPath = 'typing';
+import packageJson from '../package.json';
+let currentPath = "";
+let link = packageJson.homepage;
 
 function App() {
   return (
     <>
     <NavBar />
-    <BrowserRouter>
       <Routes>
         <Route path = {currentPath + '/'} element={<Home />} />
         <Route path = {currentPath + '/rus'} element={<RusTrainer />} />
         <Route path = {currentPath + '/eng'} element={<EngTrainer />} />
       </Routes>
-    </BrowserRouter>
     </>
   );
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,17 +1,18 @@
 import './styles.css'
 import React from 'react'
+let currentPath = "";
 
 function NavBar () {
     return (
         <ul>
             <li>
-                <a href = '/'>Home</a>
+                <a href = {currentPath + '/'}>Home</a>
             </li>
             <li>
-                <a href = '/rus'>Rus</a>
+                <a href = {currentPath + '/rus'}>Rus</a>
             </li>
             <li>
-                <a href = '/eng'>Eng</a>
+                <a href = {currentPath + '/eng'}>Eng</a>
             </li>
         </ul>
     );
