62e008c47adec32bd754797b2106733d47d72e0d
buserqub <73015330+buserqub@users.noreply.github.com>
2023-10-09 20:54:29 +0500

    Д

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,8 @@ import React from 'react'
 import slide1 from './images/slide1.png'
 import slide2 from './images/slide2.png'
 import slide3 from './images/slide3.png'
+import fiva from './images/ФЫВАОЛДЖ.png'
+import speed from './images/Скоропись.png'
 
 function Home () {
     return (
@@ -15,7 +17,6 @@ function Home () {
                     <input type="radio" name = "r" id="r1"></input>
                     <input type="radio" name = "r" id="r2"></input>
                     <input type="radio" name = "r" id="r3"></input>
-                    <input type="radio" name = "r" id="r4" defaultChecked></input>
                     <div className = "slides s1">
                         <div className = "slide">
                             <div>
@@ -40,6 +41,32 @@ function Home () {
                         <label htmlFor = "r1" className = "bar" id = "lb1"></label>
                         <label htmlFor = "r2" className = "bar" id = "lb2"></label>
                         <label htmlFor = "r3" className = "bar" id = "lb3"></label>
+                    </div>
+                </div>
+                <div className='content middle'>
+                    <div className='upperBlock'>
+                        <div className='leftBlock'>
+                            <img src = {fiva}></img>
+                        </div>
+                        <div className='rightBlock'>
+                            <p>"ФЫВА ОЛДЖ" - современный подход к расположению пальцев на клавиатуре, призванный
+                                увеличить эфективность скоростной слепой печати. Данный подход подразумевает под собой
+                                такое расположение пальцев, при котором пальцы левой руки от мизинца до указательного занимают позиции
+                                "ФЫВА" и отвечают за нажатие клавишь в соответствующих вертикальных последовательностях, а пальцы
+                                правой руки от указательного до мизинца - "ОЛДЖ" соответственно.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='bottomBlock'>
+                        <div className='leftBlock'>
+                            <p>Обученный такой методике набора пользователь может достигать скорости нажатий до 300-400
+                                символов в минуту, при этом текущие зафиксированные рекорды скоростной печати составляют 900
+                                символов в минуту для зранее неизвестного текста и 1200 для заранее известного.
+                            </p>
+                        </div>
+                        <div className='rightBlock'>
+                            <img src = {speed}></img>
+                        </div>
                     </div>
                 </div>
             </div>
